c52a782dcb9514d3bb4401c4d940dac78db91a7b
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-02-17 18:53:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,4 +7,15 @@ function calculateMinCost() {
 	convertedVal.sort((a, b)=> a - b);
 
 	let totalCost = 0;
+
+	while (convertedVal.length > 1) {
+        		let a = convertedVal.shift();
+		let b = convertedVal.shift();
+
+		totalCost += a + b;
+
+		convertedVal.push(a + b);
+		convertedVal.sort((a, b)=> a - b);
+	}
+	document.getElementById('result').innerHTML = totalCost;
 }  
