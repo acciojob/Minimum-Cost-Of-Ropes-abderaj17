f55719cafb9f0a762740e3d7b6d2c70f2e2c64e2
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-02-17 18:49:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+
 function calculateMinCost() {
   //your code here
-  
-  
-  
+  let value = document.getElementById('rope-lengths').value.split(',');
+	let convertedVal = value.map(Number);
+
+	convertedVal.sort((a, b)=> a - b);
+
+	let totalCost = 0;
 }  
