092dce5c248229b51b86be0d2207dc3d5d4c2d64
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-02-17 19:02:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
 function calculateMinCost() {
   let value = document.getElementById('rope-lengths').value.split(',');
-  let minHeap = value.map(Number);
+  let minHeap = new MinHeap();
 
-  // Convert array into a Min Heap
-  minHeap.sort((a, b) => a - b); // Initial sorting
+  // Convert input into numbers and add to the min heap
+  value.map(Number).forEach(num => minHeap.push(num));
 
   let totalCost = 0;
 
-  while (minHeap.length > 1) {
-    // Always remove the two smallest elements
-    let a = minHeap.shift();
-    let b = minHeap.shift();
+  while (minHeap.size() > 1) {
+    let a = minHeap.pop();
+    let b = minHeap.pop();
     
     let cost = a + b;
     totalCost += cost;
 
-    // Insert back into heap and maintain sorting
     minHeap.push(cost);
-    minHeap.sort((a, b) => a - b); // Maintain heap order
   }
 
   document.getElementById('result').innerHTML = totalCost;
