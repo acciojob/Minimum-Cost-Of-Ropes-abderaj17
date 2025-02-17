6cefd0d36acd7f4031ea8974f35061a80cd3b955
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-02-17 18:55:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-
 function calculateMinCost() {
   //your code here
   let value = document.getElementById('rope-lengths').value.split(',');
-	let convertedVal = value.map(Number);
+  let convertedVal = value.map(Number);
 
-	convertedVal.sort((a, b)=> a - b);
+  // Sort the array only once at the beginning
+  convertedVal.sort((a, b) => a - b);
 
-	let totalCost = 0;
+  let totalCost = 0;
 
-	while (convertedVal.length > 1) {
-        		let a = convertedVal.shift();
-		let b = convertedVal.shift();
+  while (convertedVal.length > 1) {
+    // Instead of using shift, find and remove the smallest element
+    let a = findAndRemoveSmallest(convertedVal);
+    let b = findAndRemoveSmallest(convertedVal);
 
-		totalCost += a + b;
+    totalCost += a + b;
 
-		convertedVal.push(a + b);
-		convertedVal.sort((a, b)=> a - b);
-	}
-	document.getElementById('result').innerHTML = totalCost;
-}  
+    convertedVal.push(a + b);
+    // No need to sort the array here
+  }
+
+  document.getElementById('result').innerHTML = totalCost;
+}
